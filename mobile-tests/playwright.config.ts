813890a3e534980059cf65701273298dfbb01692
jjhelguero <56001598+jjhelguero@users.noreply.github.com>
2024-06-28 19:49:24 -0500

    update configs

--- mobile-tests/playwright.config.ts
+++ mobile-tests/playwright.config.ts
@@ -24,11 +24,12 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    actionTimeout: 10 * 1000,
+    baseURL: 'http://localhost:5173/',
+    navigationTimeout: 30 * 1000,
+    screenshot: 'only-on-failure',
     trace: 'on-first-retry',
+    video: 'on-first-retry',
   },
 
   /* Configure projects for major browsers */
